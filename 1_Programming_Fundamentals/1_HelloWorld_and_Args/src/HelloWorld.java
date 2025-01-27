2c6a9863ac64276824cdc7bf7f907538878cfb8f
tb-harris <tbrandonharris@riverdale.edu>
2025-01-27 09:51:50 -0500

    updated arguments instructions

--- 1_Programming_Fundamentals/1_HelloWorld_and_Args/src/HelloWorld.java
+++ 1_Programming_Fundamentals/1_HelloWorld_and_Args/src/HelloWorld.java
@@ -14,11 +14,23 @@ public class HelloWorld {
         System.out.println("Line 3");
 
         /*
-        The args array stores parameters provided by the user
+        The args array stores arguments provided by the user.
+
         Example: word1 word2 word3
         args[0] <- word1
         args[1] <- word2
         args[2] <- word3
+
+        Arguments are an example of *user input* - they allow
+        the person running our program to provide new information that
+        changes its behavior (without having to edit the code).
+
+        In IntelliJ, you can run a program with arguments by:
+        1) With your class file (eg HelloWorld) open, click on the
+           ⋮ button (vertical ...) next to the run button at the top right
+        2) Click "Run with parameters" or "Edit configuration"
+        3) In the "Program Arguments" box, type the arguments separated by spaces
+        4) Press "Run", or press "Apply" and then run the program with the green button
         ...
 
         These parameters are strings. We can add (concatenate)
